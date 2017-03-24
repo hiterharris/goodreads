2b20c0fca7cf3cb0fe5b27dc5ba88cc3bafab136
hiterharris <HiterHarrisIV@Hs-MacBook-Pro.local>
2017-03-24 16:21:28 -0400

    toggle buttons added

--- js/script.js
+++ js/script.js
@@ -11,9 +11,51 @@ $(function () {
       title: 'Harry Potter',
       author: 'JK Rowling',
       cover: 'harrypotter'
+    },{
+      title: 'War and Peace',
+      author: 'Leo Tolstoy',
+      cover: 'warandpeace'
+    },{
+        title: 'The Road',
+        author: 'Cormac McCarthy',
+        cover: 'theroad'
+    },{
+        title: 'White Fang',
+        author: 'Jack London',
+        cover: 'whitefang'
+    },{
+        title: 'The Outsiders',
+        author: 'S.E. Hinton',
+        cover: 'theoutsiders'
+
     }]
   };
-  
+
   var compiled = template(context);
   $('.top-reads').append(compiled);
+});
+
+// ----------Toggle-------------
+$(document).ready(function(){
+  $('.toggle-current').hide();
+
+  $('.toggle-current-button').click(function(){
+    $('.toggle-current').slideToggle('slow');
+  })
+});
+
+$(document).ready(function(){
+  $('.toggle-queue').hide();
+
+  $('.toggle-queue-button').click(function(){
+    $('.toggle-queue').slideToggle('slow');
+  })
+});
+
+$(document).ready(function(){
+  $('.toggle-read').hide();
+
+  $('.toggle-read-button').click(function(){
+    $('.toggle-read').slideToggle('slow');
+  })
 });
